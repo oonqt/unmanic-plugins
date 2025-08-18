9f8fcb50bad5a7f33778af75b1ab27c2207a2cb9
oonqt <r0blox1053@outlook.com>
2025-08-18 12:25:23 -0700

    T

--- source/remove_commentary_soultaco83/plugin.py
+++ source/remove_commentary_soultaco83/plugin.py
@@ -51,7 +51,7 @@ class PluginStreamMapper(StreamMapper):
 
         ]
 
-        print(stream_info);
+        print(stream_info); # should work
 
         for tag in data_stream_tags:
             try:
