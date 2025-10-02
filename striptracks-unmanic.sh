e665069551ad458d5f4cc1ea328141a83bc017b9
oonqt <r0blox1053@outlook.com>
2025-10-02 00:36:57 -0700

    fix renamed files not being scanned by unmanic

--- striptracks-unmanic.sh
+++ striptracks-unmanic.sh
@@ -741,7 +741,8 @@ function rename_videofile {
   # Trigger unmanic and prevent triggered again at program exit (default behavior)
   was_renamed=1
   echo "Triggering unmanic process for renamed video file" | log
-  trigger_unmanic "$newname"
+  # $newname doesnt include the complete path, but we can get the complete path from the original video passed in by *arrs
+  trigger_unmanic "$(dirname "$striptracks_video")/$newname"
 
   [ "$striptracks_result" != "null" ] && [ "$striptracks_result" != "" ]
   return
